03d2f5ed3f162930de5f1f2ac4dc30abd3838470
asma <asmaaudah@gmail.com>
2024-05-23 16:51:45 +0700

    clean comments

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -10,11 +10,9 @@ describe('1to50 website', () => {
 
   it('Automation', () => {
     for (let i = 1; i <= 50; i++) {
-        // Gunakan XPath untuk menemukan elemen <div> yang mengandung teks tepat angka i dan mengkliknya
+        
         cy.xpath(`//*[@id="grid"]/div[normalize-space(text())="${i}"]`).click();}
 
-    //cy.get('[data-testid="product-pill-Car Rental"]').should("be.visible")
-    //cy.get('[data-testid="product-pill-Car Rental"]').click({force: true})
     
   })
 
